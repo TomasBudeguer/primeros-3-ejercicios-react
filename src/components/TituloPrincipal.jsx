cea4f17ff2b60f4452d31a4b63dde73cf0495281
Tomas <tomibudeguer20@gmail.com>
2022-09-20 19:12:05 -0300

    hice el ejercicio 3

--- src/components/TituloPrincipal.jsx
+++ src/components/TituloPrincipal.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 const TituloPrincipal = (props) => {
     // aca va la logica
+
+    const [clave, setClave] = useState('')
+
+    const mostrar = ()=>{
+        setClave('(from changed state)')
+    }
+
     return (
         // aca va el maquetado y poca logica
-        <h1>Hello {props.myFriend}!</h1>
+        <div>
+            <h1>Hello {props.myFriend} {clave}!</h1>
+            <button onClick={mostrar}>Mostrar clave</button>
+        </div>
     );
 };
 
